e90fc33720677746d630452cfa7b5394f1eddb32
Maikx <51028649+Maikx@users.noreply.github.com>
2020-11-16 20:14:27 +0100

    Add files via upload

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    static Animator anim;
+    private Rigidbody2D rB;
+    public float movementSpeed;
+
+    void Start()
+    {
+        anim = GameObject.Find("Staraptor").GetComponent<Animator>();
+        rB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckStatus();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        HandleMovement(horizontal);
+    }
+
+    void HandleMovement(float horizontal)
+    {
+        rB.velocity = new Vector2 (horizontal * movementSpeed, rB.velocity.y);
+    }
+
+    public void CheckStatus()
+    {
+        if (Input.GetKey(KeyCode.S))
+            anim.SetBool("isWalkingForward", true);
+        else
+            anim.SetBool("isWalkingForward", false);
+    }
+}
