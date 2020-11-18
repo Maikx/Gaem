379189013bbe73b0f79a881f1aae98924e77101d
Maikx <mikeybee2000@gmail.com>
2020-11-18 13:45:38 +0100

    Simple enemy ai

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+
+    private Transform size;
+    private Transform target;
+    
+    static Animator animGreed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        animGreed = GameObject.Find("Greed").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChasePlayer();
+    }
+
+    void ChasePlayer()
+    {
+        if (Vector2.Distance(transform.position, target.position) > 2)
+        {
+            animGreed.SetBool("isWalking", true);
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            animGreed.SetBool("isWalking", false);
+        }
+
+        if(target.position.x < transform.position.x)
+        {
+            transform.eulerAngles = Vector3.up * 180;
+        }
+        else
+        {
+            transform.eulerAngles = Vector3.up * 0;
+        }
+
+        if (target.position.y < transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0.1f);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -0.1f);
+        }
+    }
+}
