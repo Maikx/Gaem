5f456533e9bca1221675a65d2d020dbd5ccbb5d6
Maikx <mikeybee2000@gmail.com>
2020-11-18 23:28:09 +0100

    Sistemato il piccolo intoppo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Animator currentAnimator;
-    private SpriteRenderer spriterenderer;
+    private Animator animPlayer;
+    private SpriteRenderer sR;
+    private Rigidbody2D rB;
+
     public Sprite staraptor_Sprite;
+    public RuntimeAnimatorController animStaraptor;
+
     public Animator animPredator;
     public Sprite predator_Sprite;
-    private Rigidbody2D rB;
+
     private GameManager gm;
-    private GameObject highPoint_GO;
-    private GameObject lowPoint_GO;
 
     private float timeBtwAttack;
     public float startTimeBtwAttack;
@@ -23,46 +25,77 @@ public class Player : MonoBehaviour
     
     //public Animator vet[4];
     //public Animator[] vet = new Animator[4];
+
+    public float health;
+    public int character;
     public float movementSpeed;
+
+    public float playerHigh;
+    public float playerLow;
+
+    //cose già aggiunte..
+
     //public int meleeDamage;
     //public float meleeRange;
-    public float health;
-
-    public int character;
 
     //0 = Knight, 1 = Predator, 2 = Staraptor, 3 = Mystic, 4 = Fox
 
     void Start()
     {
-
-        //if(character == 2)
-        currentAnimator = GetComponent<Animator>();
-        spriterenderer = GetComponent<SpriteRenderer>();
-        if (character == 2)
-        {
-            spriterenderer.sprite = staraptor_Sprite;
-            
-        }
-            
+        animPlayer = GetComponent<Animator>();
+        sR = GetComponent<SpriteRenderer>();
         //vet[1]=animStaraptor = GameObject.Find("Staraptor").GetComponent<Animator>(); 
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
         rB = GetComponent<Rigidbody2D>();
-        highPoint_GO = GameObject.Find("High");
-        lowPoint_GO = GameObject.Find("Low");
     }
 
     // Update is called once per frame
     void Update()
     {
-       // CheckStatus();
-        //Boundaries();
+        // CheckStatus();
+        Boundaries();
+        PickCharacter();
         Attack();
     }
-    int PickCharacter()
+
+    void PickCharacter()
     {
-        character = 1;
-        return character;
+        if (character == 0)
+        {
+            //knight
+        }
+
+        if (character == 1)
+        {
+            //predator
+        }
+
+        if (character == 2)
+        {
+            sR.sprite = staraptor_Sprite;
+            animPlayer.runtimeAnimatorController = animStaraptor;
+        }
+
+        if (character == 3)
+        {
+            //mystic
+        }
+
+        if (character == 4)
+        {
+            //fox
+        }
+
     }
+
+
+    //int PickCharacter()
+    //{
+        //character = 2;
+        //return character;
+    //}
+
+
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
@@ -77,42 +110,45 @@ public class Player : MonoBehaviour
         rB.velocity = new Vector2(rB.velocity.x, vertical * movementSpeed);
     }
 
-    /*public void CheckStatus()
+    public void CheckStatus()
     {
-        int pos = PickCharacter();
-        staraptor_GO.SetActive(true);
-        //if (character == 2)
-        //{
-            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
-                vet[pos].SetBool("isWalkingForward", true);
-            else
-                vet[pos].SetBool("isWalkingForward", false);
+        //int pos = PickCharacter();
 
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-                vet[pos].SetBool("isWalkingLeft", true);
-            else
-                vet[pos].SetBool("isWalkingLeft", false);
 
-            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-                vet[pos].SetBool("isWalkingRight", true);
-            else
-                vet[pos].SetBool("isWalkingRight", false);
-            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-                vet[pos].SetBool("isWalkingBack", true);
-            else
-                vet[pos].SetBool("isWalkingBack", false);
-        //}
+
+
+        if (character == 2)
+        {
+            //if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                //vet[pos].SetBool("isWalkingForward", true);
+            //else
+                //vet[pos].SetBool("isWalkingForward", false);
+
+            //if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                //vet[pos].SetBool("isWalkingLeft", true);
+            //else
+                //vet[pos].SetBool("isWalkingLeft", false);
+
+            //if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                //vet[pos].SetBool("isWalkingRight", true);
+            //else
+                //vet[pos].SetBool("isWalkingRight", false);
+            //if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                //vet[pos].SetBool("isWalkingBack", true);
+            //else
+                //vet[pos].SetBool("isWalkingBack", false);
+        }
     }
-    */
+    
     void Boundaries()
     {
-        if (transform.position.y > highPoint_GO.transform.position.y)
+        if (transform.position.y > playerHigh)
         {
-            transform.position = new Vector3(transform.position.x, highPoint_GO.transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x, playerHigh, transform.position.z);
         }
-        if (transform.position.y < lowPoint_GO.transform.position.y)
+        if (transform.position.y < playerLow)
         {
-            transform.position = new Vector3(transform.position.x, lowPoint_GO.transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x, playerLow, transform.position.z);
         }
     }
 
